1c4eb724e51adf962679ae993b52ab4e9d07b1b2
Mauricio Dias <mauriciocd7@gmail.com>
2020-11-30 18:53:43 -0300

    Commit inicial exercício de array

---
+++ Exercicio_Pratico_Variaveis_Arrays/index.php
@@ -0,0 +1,26 @@
+<?php //Exercício de Revisão Arrays em php
+
+    $lista = [
+        'nome' => 'Maurício',
+        'idade' => '28',
+        'atributos' => [
+            'forca' => 60,
+            'agilidade' => 80,
+            'destreza' => 50
+        ],
+        'vida' => 1000,
+        'mana' => 928,
+    ];
+
+
+    
+    echo "Nome: ".$lista['nome']."<br>";    
+    echo "Idade: ".$lista['idade']."<br>";
+    echo "Força: ".$lista['atributos']['forca']."<br>";
+    echo "Agilidade: ".$lista['atributos']['agilidade']."<br>";
+    echo "Destreza: ".$lista['atributos']['destreza']."<br>";
+    echo "Vida: ".$lista['vida']."<br>";    
+    echo "Mana: ".$lista['mana']."<br>";
+
+    echo '<pre>'; print_r($lista); echo '<pre/>';
+?>
