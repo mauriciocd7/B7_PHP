953f2fdac079113d643984ee3562becdc34130a5
Mauricio Dias <mauriciocd7@gmail.com>
2020-11-30 23:07:23 -0300

    Exercicio 2

---
+++ Condicionais_Loops/exercicio2.php
@@ -0,0 +1,11 @@
+<?php //Utilizar loops
+
+    for ($a=0; $a <= 20; $a++) {  //correção
+
+        for($b=0; $b < $a; $b++) { // $b será menor que $a e repetirá - até sair do laço
+        echo "-";
+        }
+    echo "</br>";
+    }
+
+?>
